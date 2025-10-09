a620cfc1ab8702fc404fc53312bddf9e5876856d
Diego <dpadula@epe.santafe.gov.ar>
2025-10-09 13:53:50 -0300

    Incorporamos detalles con ruteo desde el item de la lista

---
+++ src/app/meditation/details.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const MeditationDetails = () => {
+  return (
+    <View>
+      <Text>MeditationDetails</Text>
+    </View>
+  );
+};
+
+export default MeditationDetails;
+
+const styles = StyleSheet.create({});
--- src/components/MeditationListItem.tsx
+++ src/components/MeditationListItem.tsx
@@ -1,8 +1,9 @@
 import { Meditation } from '@/types';
 import AntDesign from '@expo/vector-icons/AntDesign';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Link } from 'expo-router';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 type MeditationListItemProps = {
   meditation: Meditation;
@@ -10,20 +11,24 @@ type MeditationListItemProps = {
 
 const MeditationListItem = ({ meditation }: MeditationListItemProps) => {
   return (
-    <View className='flex-row items-center gap-5'>
-      <View className='bg-green-700 p-2 rounded-full'>
-        <FontAwesome name='check' size={16} color='white' />
-      </View>
-      <View className='flex-1 p-10 border border-gray-300 rounded-2xl '>
-        <Text className='text-xl font-semi-bold mb-3'>{meditation.title}</Text>
-        <View className='flex-row items-center gap-2 mt-2'>
-          <AntDesign name='clock-circle' size={18} color='#6B7280' />
-          <Text className='text-gray-600 font-semi-bold'>
-            {meditation.duration} min
-          </Text>
+    <Link href={`/meditation/details/`} asChild>
+      <Pressable className='flex-row items-center gap-5'>
+        <View className='bg-green-700 p-2 rounded-full'>
+          <FontAwesome name='check' size={16} color='white' />
         </View>
-      </View>
-    </View>
+        <View className='flex-1 p-10 border border-gray-300 rounded-2xl '>
+          <Text className='text-xl font-semi-bold mb-3'>
+            {meditation.title}
+          </Text>
+          <View className='flex-row items-center gap-2 mt-2'>
+            <AntDesign name='clock-circle' size={18} color='#6B7280' />
+            <Text className='text-gray-600 font-semi-bold'>
+              {meditation.duration} min
+            </Text>
+          </View>
+        </View>
+      </Pressable>
+    </Link>
   );
 };
 
