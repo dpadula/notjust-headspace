f5d84c8a299b7172fceca654dac607e9d2764b48
Diego <dpadula@epe.santafe.gov.ar>
2025-10-09 12:10:25 -0300

    acomodado para que funcione nativewind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,7 +5,9 @@ export default function HomeScreen() {
     <View style={styles.container}>
       <View style={styles.main}>
         <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
+        <Text className='text-4xl font-bold p-10'>
+          This is the first page of your app.
+        </Text>
       </View>
     </View>
   );
@@ -26,9 +28,5 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 64,
     fontWeight: 'bold',
-  },
-  subtitle: {
-    fontSize: 36,
-    color: '#38434D',
   },
 });
