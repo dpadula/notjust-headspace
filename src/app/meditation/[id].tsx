72acde27304643d9db2246cdd5cbba47a5e79f30
Diego <dpadula@epe.santafe.gov.ar>
2025-10-09 14:24:30 -0300

    Mejorado del estilo de details screen

--- src/app/meditation/[id].tsx
+++ src/app/meditation/[id].tsx
@@ -3,9 +3,14 @@ import AntDesign from '@expo/vector-icons/AntDesign';
 import { router, useLocalSearchParams } from 'expo-router';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  SafeAreaView,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
 
 const MeditationDetails = () => {
   const { id } = useLocalSearchParams<{ id: string }>();
+  const { top } = useSafeAreaInsets();
 
   const meditation = meditations.find(
     (meditation) => meditation.id === Number(id)
@@ -18,18 +23,19 @@ const MeditationDetails = () => {
     );
   }
   return (
-    <>
+    <SafeAreaView className='flex-1 bg-white px-4'>
       <Text className='text-2xl font-bold mt-16'>{meditation?.title}</Text>
       <AntDesign
         onPress={() => {
           router.back();
         }}
-        className='absolute top-16 right-4'
+        className='absolute right-4'
+        style={{ top: top + 16 }}
         name='close'
         size={24}
         color='black'
       />
-    </>
+    </SafeAreaView>
   );
 };
 
