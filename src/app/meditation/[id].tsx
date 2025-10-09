8cceeafc9f0d45c41ad05a69ae23aa640500fd98
Diego <dpadula@epe.santafe.gov.ar>
2025-10-09 14:03:57 -0300

    Mejorado de lectura de datos de los detalles segun navegacion

--- src/app/meditation/[id].tsx
+++ src/app/meditation/[id].tsx
@@ -1,12 +1,24 @@
+import { meditations } from '@/data';
 import { useLocalSearchParams } from 'expo-router';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 const MeditationDetails = () => {
   const { id } = useLocalSearchParams<{ id: string }>();
+
+  const meditation = meditations.find(
+    (meditation) => meditation.id === Number(id)
+  );
+  if (!meditation) {
+    return (
+      <View>
+        <Text className='text-2xl font-bold'>Meditation not found</Text>
+      </View>
+    );
+  }
   return (
     <View>
-      <Text>Meditation Details: {id}</Text>
+      <Text className='text-2xl font-bold'>{meditation?.title}</Text>
     </View>
   );
 };
