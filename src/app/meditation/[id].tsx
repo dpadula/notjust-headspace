5c1570fdac7b0d18c11669d7721c0ad8a10b1a3e
Diego <dpadula@epe.santafe.gov.ar>
2025-10-13 10:33:31 -0300

    Diseño general de la pantalla de detalle

--- src/app/meditation/[id].tsx
+++ src/app/meditation/[id].tsx
@@ -1,10 +1,11 @@
 import { meditations } from '@/data';
 import AntDesign from '@expo/vector-icons/AntDesign';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import { router, useLocalSearchParams } from 'expo-router';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-
 const MeditationDetails = () => {
   const { id } = useLocalSearchParams<{ id: string }>();
 
@@ -19,26 +20,51 @@ const MeditationDetails = () => {
     );
   }
   return (
-    <SafeAreaView className='flex-1 bg-orange-400'>
+    <SafeAreaView className='flex-1 bg-orange-400 justify-between'>
       {/* Header */}
-
-      <View className='flex-row items-center justify-between px-10 mt-4'>
-        <AntDesign name='info-circle' size={24} color='black' />
-        <View className='bg-zinc-700 rounded-md p-2'>
-          <Text className='text-zinc-100 font-bold'>Today's meditation</Text>
+      <View className='flex-1'>
+        <View className='flex-row items-center justify-between px-10 mt-4'>
+          <AntDesign name='info-circle' size={24} color='black' />
+          <View className='bg-zinc-700 rounded-md p-2'>
+            <Text className='text-zinc-100 font-bold'>Today's meditation</Text>
+          </View>
+          <AntDesign
+            onPress={() => {
+              router.back();
+            }}
+            name='close'
+            size={22}
+            color='black'
+          />
         </View>
-        <AntDesign
-          onPress={() => {
-            router.back();
-          }}
-          name='close'
-          size={22}
-          color='black'
-        />
+        <Text className='text-3xl text-center text-zinc-800 font-semibold mt-10'>
+          {meditation?.title}
+        </Text>
       </View>
-      <Text className='text-3xl text-center text-zinc-800 font-semibold mt-10'>
-        {meditation?.title}
-      </Text>
+      {/* Play and Pasuse Button */}
+      <Pressable className='bg-zinc-800 self-center items-center justify-center w-20 aspect-square rounded-full mt-10'>
+        <FontAwesome6 name='play' size={24} color='snow' />
+      </Pressable>
+      {/* <View className='bg-zinc-800 self-center items-center justify-center w-20 aspect-square rounded-full mt-10'>
+        <FontAwesome6 name='pause' size={24} color='snow' />
+      </View> */}
+      {/* Player */}
+      <View className='flex-1'>
+        <View className='p-10 mt-auto'>
+          <View className='flex-row items-center justify-between'>
+            <MaterialCommunityIcons
+              name='broadcast'
+              size={32}
+              color='#3A3937'
+            />
+            <MaterialCommunityIcons
+              name='cog-outline'
+              size={32}
+              color='#3A3937'
+            />
+          </View>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
